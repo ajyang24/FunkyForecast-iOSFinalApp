aaaf50dd285d4221089cd8407f7dd8c7b68b6bec
Andrew Yang <ayang@andrews-macbook-air.local>
2017-06-29 13:10:37 -0500

    Commit

--- FunkyForecast/ViewController.swift
+++ FunkyForecast/ViewController.swift
@@ -11,53 +11,51 @@ import UIKit
 
 class ViewController: UIViewController
 {
+
     var locations = [[String: String]]()
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        let currentWeatherUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/conditions/q/IL/Barrington.json"
-        let hourlyWeatherUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/hourly/q/IL/Barrington.json"
-        let sevenDayForecastUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/forecast7day/q/IL/Chicago.json"
-        if let url = URL(string: currentWeatherUrl)
-        {
-            if let myData = try? Data(contentsOf: currentWeatherUrl, options: [])
-            {
-                let json = JSON(myData)
-                parse(myData: json)
-            }
-            
-        }
-        if let url1 = URL(string: hourlyWeatherUrl)
-        {
-            if let myDataHourly = try? Data(contentsOf: hourlyWeatherUrl, options: [])
-            {
-                let json = JSON(myDataHourly)
-                parse(myDataHourly: json)
-            }
-            
-        }
-        
-        if let url3 = URL(string: sevenDayForecastUrl)
-        {
-            if let myDataWeekly = try? Data(contentsOf: sevenDayForecastUrl, options: [])
-            {
-                let json = JSON(myDataWeekly)
-                parse(myDataWeekly: json)
-            }
-            
-        }
+//        let currentWeatherUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/conditions/q/IL/Barrington.json"
+//        let hourlyWeatherUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/hourly/q/IL/Barrington.json"
+//        let sevenDayForecastUrl = "http://api.wunderground.com/api/bf7798dd77b9bf97/forecast7day/q/IL/Chicago.json"
+//        if let url = URL(string: currentWeatherUrl)
+//        {
+//            if let myData = try? Data(contentsOf: currentWeatherUrl, options: [])
+//            {
+//                let json = JSON(myData)
+//                parse(myData: json)
+//            }
+//            
+//        }
+//        if let url1 = URL(string: hourlyWeatherUrl)
+//        {
+//            if let myDataHourly = try? Data(contentsOf: hourlyWeatherUrl, options: [])
+//            {
+//                let json = JSON(myDataHourly)
+//                parse(myDataHourly: json)
+//            }
+//            
+//        }
+//        
+//        if let url3 = URL(string: sevenDayForecastUrl)
+//        {
+//            if let myDataWeekly = try? Data(contentsOf: sevenDayForecastUrl, options: [])
+//            {
+//                let json = JSON(myDataWeekly)
+//                parse(myDataWeekly: json)
+//            }
+//            
+//        }
         
         
 
 
 
     }
-    
-    func parse(myData:JSON)
-    {
-        for i in myData[
-    }
+
+
 
 
 
